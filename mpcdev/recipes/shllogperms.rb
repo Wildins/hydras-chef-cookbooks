abffa9e03710db95e87dc2c588dbb4c879fa95fc
wildins <stephen.wilding@hotmail.co.uk>
2014-06-19 17:02:59 -0400

    Updated shllogperms

--- mpcdev/recipes/shllogperms.rb
+++ mpcdev/recipes/shllogperms.rb
@@ -1,7 +1,6 @@
 node[:deploy].each do |application, deploy|
-  36du387_log_root = "#{deploy[:deploy_to]}/current/shl/36du387"
-  gather_log_root = "#{deploy[:deploy_to]}/current/shl/gather"
-  execute "chmod -R g+rw #{36du387_log_root} #{gather_log_root}" do
+  log_root = "#{deploy[:deploy_to]}/current/shl"
+  execute "chmod -R g+rw #{log_root}" do
     only_if do
        File.directory?("#{deploy[:deploy_to]}/current/shl")
     end
